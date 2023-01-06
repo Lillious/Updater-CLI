2de77939e0b1ed2a6e94aacee972132f9e2bb14a
Logan Brown <logan.brown@tylertech.com>
2023-01-06 13:42:48 -0500

    Initial Release

---
+++ app.js
@@ -0,0 +1,84 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const crash = (err) => { console.error(`\x1b[31m${err}\x1b[0m`); process.exit(1); };
+const path = require('path');
+const fs = require('fs');
+
+const url = app.commandLine.getSwitchValue("url");
+const installPath = app.commandLine.getSwitchValue("install");
+const executablePath = app.commandLine.getSwitchValue("executablepath");
+const executable = app.commandLine.getSwitchValue("executable");
+
+// Delete temp config file if it exists
+if (fs.existsSync (path.join(__dirname, 'config.json'), (err) => { crash(err); })) {
+    fs.unlinkSync(path.join(__dirname, 'config.json'));
+}
+
+// Create temp config file with arguments
+const config = {
+    "url": url,
+    "installPath": installPath,
+    "executablePath": executablePath,
+    "executable": executable
+};
+
+// Write config file
+fs.writeFileSync(path.join(__dirname, 'config.json'), JSON.stringify(config), (err) => {crash(err); });
+
+// Attempt to delete temp config file on exit
+process.on('exit', () => {
+    fs.unlinkSync(path.join(__dirname, 'config.json'));
+});
+
+// If the app crashes, delete the temp config file
+process.on('uncaughtException', (err) => {
+    fs.unlinkSync(path.join(__dirname, 'config.json'));
+    crash(err);
+});
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 600,
+        minWidth: 600,
+        maxWidth: 600,
+        height: 250,
+        minHeight: 250,
+        maxHeight: 250,
+        frame: false,
+        darkTheme: true,
+        resizable: true,
+        webPreferences: {
+            nodeIntegration: true,
+            nodeIntegrationInWorker: true,
+            contextIsolation: false,
+            sandbox: false,
+            spellcheck: false,
+            enableRemoteModule: true,
+        }
+    });
+    win.loadFile(`./src/index.html`)
+    .catch((err) => { crash(err); });
+}
+
+app.whenReady().then(() => {
+    createWindow();
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+})
+.catch((err) => { crash(err); });
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+ipcMain.on('close', () => {
+    app.quit();
+});
+
+ipcMain.on('minimize', () => {
+    BrowserWindow.getAllWindows()[0].minimize();
+});
---
+++ src/www/js/script.js
@@ -0,0 +1,170 @@
+const { ipcRenderer } = require('electron');
+const close = document.getElementById('close');
+const minimize = document.getElementById('minimize');
+const path = require('path');
+const request = require('request');
+const unzipper = require('unzipper');
+const fs = require('fs');
+
+close.addEventListener('click', () => {
+    ipcRenderer.send('close');
+});
+
+minimize.addEventListener('click', () => {
+    ipcRenderer.send('minimize');
+});
+
+// Read config file
+const config = require(path.join(__dirname, '../config.json'));
+const url = config.url;
+const installPath = config.installPath;
+const executablePath = config.executablePath;
+const executable = config.executable;
+
+// Check if url is a valid link
+if (url) {
+    // Check if url ends in .zip or .exe
+    if (url.endsWith('.zip') || url.endsWith('.exe')) {
+            // Download file if url is valid
+            getInstallerFile(url).then(() => {
+                // Extract package.zip file to installPath location
+                extractFiles(path.join(__dirname, '../', 'package.zip'), path.join(installPath)).then(() => {
+                    showToast('success', 'Successfully installed package');
+                    document.getElementById('toast').innerHTML = "";
+                    // delete package.zip file
+                    fs.unlinkSync(path.join(__dirname, '../', 'package.zip'), (err) => {
+                        if (err) {
+                            console.log(err);
+                            showToast('error', 'Failed to delete package.zip');
+                        }
+                    });
+                    // Run executable if the option is provided
+                    if (executable != '') {
+                        // Check if executable exists
+                        showToast('success', `Opening ${executable}`);
+                        // Run executable externally and then close the current window
+                        console.log(`cd "${executablePath}" && start ${executable}`);
+                        require('child_process').exec(`cd "${executablePath}" && start ${executable}`, (err) => {
+                            if (err) {
+                                console.log(err);
+                                showToast('error', `Failed to open ${executable}`);
+                            } else {
+                                ipcRenderer.send('close');
+                            }
+                        });
+                    } else {
+                        ipcRenderer.send('close');
+                    }
+                    
+                }).catch((err) => {
+                    console.log(err);
+                    showToast('error', 'Failed to extract installer file');
+                });
+            }).catch((err) => {
+                console.log(err);
+                showToast('error', 'Failed to download installer file');
+            });
+    } else {
+        showToast('error', 'URL is not a valid installer file');
+    }
+} else {
+    showToast('error', 'URL was not provided');
+}
+
+function showToast (mode, message) {
+    const NotificationContainer = document.createElement('div');
+    const NotificationContent = document.createElement('div');
+    const NotificationClose = document.createElement('div');
+    NotificationContainer.classList.add('notification-bar');
+    NotificationContent.classList.add('notification-content');
+    // Create id
+    NotificationContent.innerHTML = message;
+    NotificationContainer.appendChild(NotificationContent);
+    // Shift the element up by 50px for each notification that isn't on the screen
+    NotificationContainer.style.marginTop = `${50 * document.getElementsByClassName('notification-bar').length}px`;
+    if (mode === 'success') {
+        // Green
+        NotificationContainer.style.borderRight = '4px solid #238636';
+    } else if (mode === 'error') {
+        // Red
+        NotificationContainer.style.borderRight = '4px solid #ed6a5e';
+    } else if (mode === 'information') {
+        // Blue
+        NotificationContainer.style.borderRight = '4px solid #2a9d8f';
+    }
+    document.body.appendChild(NotificationContainer);
+    NotificationClose.addEventListener('click', () => {
+        document.body.removeChild(NotificationContainer);
+    });
+    setTimeout(() => {
+        document.body.removeChild(NotificationContainer);
+    }, 3000);
+}
+
+function getInstallerFile (installerfileURL) {
+    // split the url into an array of strings using the / as a separator
+    const urlArray = installerfileURL.split('/');
+    // get the last item in the array
+    const filename = urlArray[urlArray.length - 1];
+    // Variable to save downloading progress
+    var received_bytes = 0;
+    var total_bytes = 0;
+    var outStream = fs.createWriteStream('package.zip');
+    // Create new promise with the Promise() constructor;
+    return new Promise((resolve, reject) => {
+        request
+            .get(installerfileURL)
+                .on('error', function(err) {
+                    console.log(err);
+                    reject(err);
+                })
+                .on('response', function(data) {
+                    total_bytes = parseInt(data.headers['content-length']);
+                })
+                .on('data', function(chunk) {
+                    document.getElementById('toast').innerHTML = `Downloading ${filename}`;
+                    received_bytes += chunk.length;
+                    showDownloadingProgress(received_bytes, total_bytes);
+                })
+                .on('end', function() {
+                    resolve();
+                })
+                .pipe(outStream);
+    })
+};
+
+function showDownloadingProgress(received, total) {
+    var percentage = ((received * 100) / total).toFixed(1);
+    document.getElementById('progress-text').innerHTML = percentage + '%';
+    // Set progress bar width to percentage
+    document.getElementById('progress').style.width = percentage + '%';
+    if (document.getElementById('progress-text').innerHTML == '100.0%') {
+        document.getElementById('progress-text').innerHTML = '0%'
+        document.getElementById('progress-container').style.display = 'none';
+        document.getElementById('toast').innerHTML = '';
+    } else {
+        document.getElementById('progress-container').style.display = 'block';
+    }
+}
+
+function extractFiles (file, filePath) {
+    return new Promise((resolve, reject) => {
+        fs.createReadStream(file)
+        .pipe(unzipper.Parse())
+        .on('entry', function (entry) {
+            const fileName = entry.path;
+            const type = entry.type; // 'Directory' or 'File'
+            // if type is Directory, create it
+            if (type === 'Directory') {
+                fs.mkdir(path.join(filePath, fileName), (err) => {
+                    if (err) throw err;
+                });
+            } else {
+                document.getElementById('toast').innerHTML = `Extracting ${fileName}...`
+                entry.pipe(fs.createWriteStream(path.join(filePath, fileName)));
+            }
+        }).on('close', () => {
+            resolve();
+        }); 
+    });
+}
